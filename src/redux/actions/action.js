f728950e279146339cb01ea8d16aa999602af9b1
Wellington Ferreira <wantunesferreira@gmail.com>
2022-06-10 18:24:34 -0300

    remove todos os arquivos

--- src/redux/actions/action.js
+++
@@ -1,8 +0,0 @@
-export const SAVE_FORM = 'SAVE_FORM';
-
-const saveForms = (info) => ({
-  type: SAVE_FORM,
-  info,
-});
-
-export default saveForms;
--- src/redux/store/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import formsReducer from '../reducers/reducer';
-
-const store = createStore(formsReducer, composeWithDevTools());
-
-export default store;
